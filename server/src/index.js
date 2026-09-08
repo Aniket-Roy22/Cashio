04458361aa8910ce0645195e6f6f8c75fba5ae57
Aniket Roy <royaniket2212@gmail.com>
2026-09-08 16:31:20 +0530

    Ready to host backend on render

--- server/src/index.js
+++ server/src/index.js
@@ -5,7 +5,7 @@ import passport from "passport";
 import authRoutes from "./routes/authRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js"
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json({limit: "1mb"}));
@@ -13,6 +13,12 @@ app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(passport.initialize());
 
+app.get("/health", (req, res) => {
+	console.log("Server Online...");
+	res.status(200).json({
+		status: "online",
+	});
+})
 app.use("/auth", authRoutes);
 app.use("/categories", categoryRoutes)
 
