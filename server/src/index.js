ae72836086b17ecff26192c03321873076b04746
Aniket Roy <royaniket2212@gmail.com>
2026-09-09 20:33:44 +0530

    update health endpoint to check supabase activity

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,7 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import passport from "passport";
+import prisma from "./config/prisma.js";
 import authRoutes from "./routes/authRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js"
 
@@ -20,12 +21,17 @@ app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(passport.initialize());
 
-app.get("/health", (req, res) => {
-	console.log("Server Online...");
-	res.status(200).json({
-		status: "online",
-	});
-})
+app.get("/health", async (req, res) => {
+	try
+	{
+		await prisma.$queryRaw`SELECT 1`;
+		res.status(200).json({status: "System & Database Online..."});
+	}
+	catch (error)
+	{
+		res.status(503).json({status: "System Error. Health Checkup Failed..."});
+	}
+});
 app.use("/auth", authRoutes);
 app.use("/categories", categoryRoutes)
 
